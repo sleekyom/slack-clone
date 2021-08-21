fda80e04a374e7f06f644e8dac8125cb144e7247
Emmanuel Kolawole <amdas72@gmail.com>
2021-08-21 18:24:45 +0300

    chat header added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import styled from "styled-components";
 import Sidebar from "./components/Sidebar";
+import Chat from "./components/Chat";
 function App() {
 	return (
 		<div className="App">
@@ -13,7 +14,9 @@ function App() {
 					<AppBody>
 						<Sidebar />
 						<Switch>
-							<Route path="/" exact></Route>
+							<Route path="/" exact>
+								<Chat />
+							</Route>
 						</Switch>
 					</AppBody>
 				</>
